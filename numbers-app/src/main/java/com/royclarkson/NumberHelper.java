feea8338ff77b4d2357d6f44f16e8eb9cd770f60
Roy Clarkson <roy@clarkson.org>
2015-04-03 16:30:34 -0500

    Add sample apps

---
+++ numbers-app/src/main/java/com/royclarkson/NumberHelper.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.royclarkson;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestOperations;
+
+@Component
+public class NumberHelper {
+
+	@Autowired
+	RestOperations rest;
+
+	@HystrixCommand(fallbackMethod = "getDefaultNumber")
+	public Number getNumber() {
+		NumberMessage numberMessage = rest.getForObject("http://NumbersService/number", NumberMessage.class);
+		return numberMessage.getNumber();
+	}
+
+	public Number getDefaultNumber() {
+		return Long.MIN_VALUE;
+	}
+
+}
